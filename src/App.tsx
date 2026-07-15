40cb74e121296d6c16e8107fac37e1a34021b89d
Andryws Görtz <andryws.kenzie@gmail.com>
2026-07-15 17:38:51 -0300

    fix of items position

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
-import { useState} from 'react'
 import './App.css'
+import ToDoList from './Components/CreateList'
 
 function App() {
-  const [item, setItem] = useState<string>('')
-  const [list, setList]= useState<string[]>([])
-
-  function addItemToList(newItemList:string){
-    setList([...list,newItemList])
-
-
-}
+ 
   return (
     <>
     <div>
-      here will be my to-do list
-        <input
-        placeholder='Activitie'
-        onChange={(e)=>setItem(e.target.value) }></input>
-      <button onClick={()=>addItemToList(item)}>ADD</button>
-      <div>This is the last item {item}</div>
-      <div>This is your to-do list: {list.map((item, index)=>{
-        return <li key={index}>{item}</li>
-      })}</div>
+      Bellow will be your to-do list
+    </div>
+    <div>
+       <ToDoList/>
+
     </div>
     </>
   )
