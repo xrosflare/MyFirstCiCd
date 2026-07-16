14fe70b78d2a23e69ec8a244ea056ca5c3363da5
Andryws Görtz <andryws.gseixas@gmail.com>
2026-07-16 19:01:48 -0300

    feat add show activities done and edit button

--- src/Components/CreateList/index.tsx
+++ src/Components/CreateList/index.tsx
@@ -6,6 +6,7 @@ import { useState } from "react"
 export default function ToDoList (){
  const [item, setItem] = useState<string>('')
   const [list, setList]= useState<string[]>([])
+  const [doneList, setDoneList]= useState<string[]>([])
 
   function addItemToList(newItemList:string){
     setList([...list,newItemList])
@@ -15,8 +16,14 @@ export default function ToDoList (){
 function deleteItemFromList(itemToDelete:string, indexToDelete:number){
 const updatedList = list.filter((itemToDelete)=> itemToDelete != list[indexToDelete])
 setList(updatedList)
-
+setDoneList([...doneList,itemToDelete])
 }
+
+function updateTask(itemToUpdate:string, indexItem:number){
+  const updatedTask = list.filter((itemToUpdate)=> itemToUpdate != list[indexItem])
+  setList([...updatedTask, itemToUpdate])
+}
+
 return<>
  <input
         placeholder='Activitie'
@@ -25,8 +32,18 @@ return<>
       <div>This is your to-do list: {list.map((item, index)=>{
           return <li key={index}>{item}
           <button onClick={()=>deleteItemFromList(item,index)}>DONE</button>
+          <button onClick={()=>updateTask(item,index)}>EDIT</button>
           </li>
+
         })}</div>
+        
+        <div>Here is your done list:
+          {doneList.map((item,index)=>{
+            return <li key={index} >{item}</li>
+          })}
+        </div>
         </>
 
 }
+
+//next step will be to create a modal to edit the tasks when clicking edit 
