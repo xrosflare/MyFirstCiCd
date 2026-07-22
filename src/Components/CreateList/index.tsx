3bd302cb371b2b2fb7e51a658586afde813a3b7a
Andryws Görtz <andryws.gseixas@gmail.com>
2026-07-22 17:48:05 -0300

    fix the correct obtain of each object id

--- src/Components/CreateList/index.tsx
+++ src/Components/CreateList/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
+import { v4 as uuidv4 } from 'uuid';
+
 type TaskType = {
-id:number,
+id:string,
 task:string,
 done:boolean
 }
@@ -16,22 +18,23 @@ export default function ToDoList (){
 
 
 function CountId(){
-  // return Number(TaskList.length)+1
-  return 0
+ const newId = uuidv4()
+  return newId
 }
 
-function createTask (id:number, task:string,done:boolean){
+function createTask (id:string, task:string,done:boolean){
   // TaskList.push({id:id,task:task, done:done})
   setTaskList([...taskList, {id:id,task:task, done:done} ])
 }
 
-function doneTask(id:number){
+function doneTask(id:string){
  const removedTask= taskList.filter(task=> task.id != id)
  setTaskList([...removedTask])
  setDoneList([...removedTask])
 }
 
-function editTask(id:number, task:string, status:boolean){
+function editTask(id:string, task:string, status:boolean){
+
 setTaskList(taskListPrevious => taskListPrevious.map(item=> {
   if(item.id == id){
     return {...item, task:task }
@@ -47,14 +50,14 @@ return<>
         onChange={(e)=>setItem(e.target.value) }></input>
       <button onClick={()=>createTask(CountId(),item,false)}>ADD</button>
       <div>This is your to-do list: 
-        {taskList.map((item, id)=>{
-          return <li key={id}>{item.task}
+        {taskList.map((item, index)=>{
+          return <li key={item.id}>{item.task}
           <button onClick={()=>setOpen(true)}>EDIT</button>
           <button onClick={()=>setOpen(true)}>DONE</button>
           {open && (
             <>
             <input placeholder="Type the new task" onChange={(e)=>setEditedItem(e.target.value)}></input>
-            <button onClick={()=>editTask(id, editedItem,false)}>SAVE</button>
+            <button onClick={()=>editTask(item.id, editedItem,false)}>SAVE</button>
             </>
           )
           }
