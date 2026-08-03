aad615cbcc7d98fb62d7893be1c1c72fb517211c
Andryws Görtz <andryws.gseixas@gmail.com>
2026-08-03 18:18:41 -0300

    fix the correct type event, feat add localstorage of login user feat add redirect button for newacc page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,8 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import './login.css'
-
+import './login.css';
 
 import { errorTime, successTime } from '../Components/CreateList/ToDoList';
 
@@ -15,16 +14,27 @@ export function Login() {
 
   const navigate = useNavigate();
 
-  const handleLogin = (e: any) => {
+  const handleLogin = (e: React.SubmitEvent) => {
     e.preventDefault();
     if (email === defaultValidUser && senha === defaultPassword) {
-        toast.success(`Access granted, Welcome ${email}`, {
-          autoClose: successTime,
-        });
+      toast.success(`Access granted, Welcome ${email}`, {
+        autoClose: successTime,
+      });
       localStorage.setItem('@app:user', email);
-      setTimeout(() => {          
-          navigate('/dashboard');
+      setTimeout(() => {
+        navigate('/dashboard');
       }, 500);
+    }
+
+    if (localStorage.getItem('@app:newUser')) {
+      toast.success(
+        `Access granted, Welcome ${localStorage.getItem('@app:newUser')}`,
+        { autoClose: successTime },
+      );
+      setTimeout(() => {
+        navigate('/dashboard');
+      }, 500);
+      
     } else {
       setSenha('');
       setEmail('');
@@ -33,9 +43,15 @@ export function Login() {
       });
     }
   };
+  const handleNewAcc = () => {
+    toast.success('We are redirecting you', { autoClose: successTime });
+    setTimeout(() => {
+      navigate('/newAcc');
+    }, 500);
+  };
 
   return (
-    <div className='LoginContainer'>
+    <div className="LoginContainer">
       <ToastContainer
         position="top-right"
         hideProgressBar={false}
@@ -57,18 +73,28 @@ export function Login() {
         <div>
           <input
             type="password"
-            placeholder="Sua senha"
+            placeholder="Password"
             value={senha}
             onChange={(e) => setSenha(e.target.value)}
           />
         </div>
         <br />
-        <button type="submit" className='loginButton' title='login'>Entrar</button>
+        <button type="submit" className="loginButton" title="login">
+          Entrar
+        </button>
       </form>
       <br />
       <div>
-      <button className='newAccButton' title='New account'>New Acc</button>
-      <button className='recoverButton' title='Recover account'>Recover</button>
+        <button
+          className="newAccButton"
+          title="New account"
+          onClick={handleNewAcc}
+        >
+          New Acc
+        </button>
+        <button className="recoverButton" title="Recover account">
+          Recover
+        </button>
       </div>
     </div>
   );
