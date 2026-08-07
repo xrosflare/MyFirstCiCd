4bddbea0c5503bb6507675fae666c9169399df9d
Andryws Görtz <andryws.gseixas@gmail.com>
2026-08-06 23:32:03 -0300

    fix small positions adjustments and importations

--- src/Components/CreateList/Avaliator/index.tsx
+++ src/Components/CreateList/Avaliator/index.tsx
@@ -1,10 +1,11 @@
+import './avaliator.css'
 export default function FootAvaliator() {
   return (
-    <>
+    <div className='FootAvaliatorContainer'>
       <div>
         Here will be the Task Avaliator:
         <button>Avaliate</button>
       </div>
-    </>
+    </div>
   );
 }
