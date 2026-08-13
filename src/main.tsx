2cde1e58b69f8f8521d9874a3810650f7df1aec4
Andryws Görtz <andryws.gseixas@gmail.com>
2026-08-13 09:08:03 -0300

    fix router error , now using hashBrowser instead

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './pages/login.tsx';
 import { NewAccount } from './pages/NewAcc.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/newAcc" element={<NewAccount />} />
         <Route path="/dashboard" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
