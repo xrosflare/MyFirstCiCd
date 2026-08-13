fb0280900e0a86eb266b8621a1f1fa9480c0004e
Andryws Görtz <andryws.gseixas@gmail.com>
2026-08-13 08:59:50 -0300

    fix digitation error in repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import babel from '@rolldown/plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MyFirstCiCD',
+  base: '/MyFirstCiCd',
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
